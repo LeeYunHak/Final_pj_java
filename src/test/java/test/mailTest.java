6aa5d959e35272543b9c9f9b640fb9e5cdc5471a
LYH <sza0203@naver.com>
2021-06-25 09:34:54 +0900

    메일 안올라간거 추가

---
+++ src/test/java/test/mailTest.java
@@ -0,0 +1,47 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import javax.mail.internet.MimeMessage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root_context.xml")
+public class mailTest {
+	
+	@Autowired
+	private JavaMailSender mailSender;
+	
+	@Test
+	public void mailSendTest() throws Exception {
+
+		String subject = "test 메일";
+		String content = "테스트임요";
+		String from = "devyunhak@gmail.com";
+		String to = "dbsgkr23@naver.com";
+
+		try {
+			MimeMessage mail = mailSender.createMimeMessage();
+			MimeMessageHelper mailHelper = new MimeMessageHelper(mail, "UTF-8");
+
+			mailHelper.setFrom(from);
+			mailHelper.setTo(to);
+			mailHelper.setSubject(subject);
+			mailHelper.setText(content, true);
+
+
+			mailSender.send(mail);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
